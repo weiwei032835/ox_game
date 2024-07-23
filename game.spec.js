84727f263437aa06d1e179e282b7818b638a1a40
weiwei <wei>
2024-07-23 18:14:54 +0800

    程式判斷

---
+++ game.spec.js
@@ -0,0 +1,12 @@
+﻿const { sum } = require("./game");
+describe("sum", () => {
+    test("1+1", () => {
+        expect(sum(1, 1)).toBe(2);
+    });
+    test("2+2", () => {
+        expect(sum(2, 2)).toBe(4);
+    });
+    test("-100+3", () => {
+        expect(sum(-100, 3)).toBe(-97);
+    });
+});
